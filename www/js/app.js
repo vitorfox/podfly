b5f8c469c6588cbf3e0a24ed789754479d386a05
Vitor Teixeira <vitorfox@gmail.com>
2014-12-25 00:37:39 -0200

    master - Podcast list

--- www/js/app.js
+++ www/js/app.js
@@ -1,10 +1,28 @@
-  var app = angular.module('Podfly', []);
+document.addEventListener('deviceready', function onDeviceReady() {
+    angular.bootstrap(document, ['podfly']);
+}, false);
 
-  app.controller('PodcastController', function(){
-    this.podcast = podcast;
-  });
+var app = angular.module("podfly", []);
 
-  var podcast = {
-    name: 'Azurite',
-    description: 'Opa'
-  };
+app.directive('podcastList', function(){
+	return {
+		restrict:'E',
+		templateUrl: "fragments/podcast-list.html",
+		controllerAs: "podcastList",
+		controller: function() {
+
+			this.podcasts = [];
+
+			for (i=0; i < 5; i++) {
+
+				var obj = {
+					title: "Cocozinho" + i,
+					lastUpdate: Date.now(),
+					icon: 'img/square128.png'
+				};
+
+				this.podcasts.push(obj);
+			};
+		}
+	};
+});
